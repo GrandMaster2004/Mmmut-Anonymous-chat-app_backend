ece7e61736a915545c46dd84b69adb5e3824bb85
GrandMaster2004 <121103159+GrandMaster2004@users.noreply.github.com>
2025-03-03 22:51:46 +0530

    yash

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-const { Server } = require("socket.io");
-const io = new Server(process.env.PORT || 3000, {
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
   cors: {
     origin: "https://mmmut-anonymous-chat-app-frontend.vercel.app",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
     credentials: true,
   },
-  transports: ["websocket"], // Add this line to ensure WebSocket support
 });
 
 io.on("connection", (socket) => {
@@ -16,4 +20,10 @@ io.on("connection", (socket) => {
   socket.on("message", (obj) => {
     socket.broadcast.emit("sendthis", obj);
   });
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
