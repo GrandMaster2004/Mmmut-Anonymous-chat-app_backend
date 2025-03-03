819ebb275ad555f7230daa5cb291b400fca4c32f
GrandMaster2004 <121103159+GrandMaster2004@users.noreply.github.com>
2025-03-03 15:20:28 +0530

    yash

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
+const express = require("express");
 const { Server } = require("socket.io");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
 
-const io = new Server(process.env.PORT || 3000, {
+const io = new Server(server, {
   cors: {
-    origin: "https://mmmut-anonymous-chat-app-frontend.vercel.app",
+    origin: ["http://127.0.0.1:5500", "http://localhost:3000"], // Allow your client's origin
   },
+});
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
 });
 
 io.on("connection", (socket) => {
   socket.emit("check", "Hello world - Everything ok");
+  console.log("server running");
   socket.on("message", (obj) => {
     socket.broadcast.emit("sendthis", obj);
   });
 });
 
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
