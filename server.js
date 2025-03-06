ca87259f5dee86e2be8028ad8781bbc85193b82c
GrandMaster2004 <121103159+GrandMaster2004@users.noreply.github.com>
2025-03-06 12:45:30 +0530

    yash

--- server.js
+++ server.js
@@ -5,15 +5,28 @@ const io = new Server(process.env.PORT || 3000, {
     origin: [
       "http://127.0.0.1:5500",
       "https://mmmut-anonymous-chat-app-frontend.vercel.app",
+      
     ],
   },
 });
 
-io.on("connection", (socket) => {
-  console.log("connection");
+let onlineUsers = 0;
 
-  socket.emit("check", "Hello world - Everything ok");
+io.on("connection", (socket) => {
+  // console.log("connection");
+  onlineUsers++;
+
+  // Emit the updated user count to all clients
+  io.emit("onlineUsers", onlineUsers);
+
+  // socket.emit("check", "Hello world - Everything ok");
   socket.on("message", (obj) => {
     socket.broadcast.emit("sendthis", obj);
   });
+
+  socket.on("disconnect", () => {
+    onlineUsers--;
+    io.emit("onlineUsers", onlineUsers);
+  });
+  // console.log(onlineUsers);
 });
